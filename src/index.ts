de0560bccfb82094014edb3750e8c854fc6125e6
Aurélien Girault <contact@aureliengirault.dev>
2025-07-10 22:35:29 +0200

    :package: Feat(vite.config): create the config to package app

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./assets/main.css"
+import Roue from "./components/Roue.vue"
+import { useSpin } from "./composables/use-spin"
+
+export * from "./presets"
+
+export { Roue, useSpin }
+export default Roue
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import { dirname, resolve } from "node:path"
 import { fileURLToPath, URL } from "node:url"
 
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
 import vueDevTools from "vite-plugin-vue-devtools"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +16,26 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VueRoue",
+      // the proper extensions will be added
+      fileName: "vue-roue",
+    },
+    rollupOptions: {
+    // make sure to externalize deps that shouldn't be bundled
+    // into your library
+      external: ["vue"],
+      output: {
+      // Provide global variables to use in the UMD build
+      // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 })
