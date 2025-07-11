5b865e8909756e4bfb22f6e4e8d1a54310e0eb4e
Aurélien Girault <contact@aureliengirault.dev>
2025-07-11 09:17:15 +0200

    :package: Fix(package): export presets

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import "./assets/main.css"
 import Roue from "./components/Roue.vue"
 import { useSpin } from "./composables/use-spin"
+import { fibonacci, tshirts, yesNoMaybe, dice, nightActivity, lunch } from "./presets"
 
-export * from "./presets"
-
-export { Roue, useSpin }
+export { Roue, useSpin, fibonacci, tshirts, yesNoMaybe, dice, nightActivity, lunch }
 export default Roue
